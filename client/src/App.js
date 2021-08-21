5bf23205ca283d96269c0598fc02601712974edf
julius oh <juliusoh@gmail.com>
2021-08-21 07:28:14 -0700

    frontend finish

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.scss';
 import Home from './Home/Home';
+import Login from './Login/Login';
 import Register from './Register/Register';
 import Watch from './Watch/Watch';
 
 function App() {
   return (
-    <Register />
+    <Home />
   );
 }
 
